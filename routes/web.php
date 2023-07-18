ea4896de614d29f1105e9b4be9d119e017674eea
EligioBiasi <biasi.eligio@edufaimm.it>
2023-07-18 16:32:43 +0200

    hello world

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $greetings= [
+        'greet'=> 'Hello World'
+    ];
+
+    return view('home', $greetings);
 });
